528eb6c849239004c03ef8b3386ba020334a066c
hernan <hernan.fg96x@gmail.com>
2025-04-15 14:32:44 +0200

    app-layout and admin-layout views add missing filename extension

--- praxis-notes-app/routes/_private/_app/_admin/route.tsx
+++ praxis-notes-app/routes/_private/_app/_admin/route.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 
-import { AdminLayout } from '@shared/views/admin-layout';
+import { AdminLayout } from '@shared/views/admin-layout.view';
 
 // private routes
 export const Route = createFileRoute('/_private/_app/_admin')({
--- praxis-notes-app/routes/_private/_app/route.tsx
+++ praxis-notes-app/routes/_private/_app/route.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 
-import { AppLayout } from '@shared/views/app-layout';
+import { AppLayout } from '@shared/views/app-layout.view';
 
 // private routes
 export const Route = createFileRoute('/_private/_app')({
