9a53f044c0e3c5b925552a2df0882464ba494528
hernan <hernan.fg96x@gmail.com>
2025-04-16 15:29:32 +0200

    verify-email query

--- praxis-notes-app/bases/auth/api/auth.api.ts
+++ praxis-notes-app/bases/auth/api/auth.api.ts
@@ -3,16 +3,19 @@ import { endpoints } from '../../api/providers/server';
 // queries
 import { getLoggedInUser } from './get-logged-in-user.query';
 import { getBookmarkedUsers } from './get-bookmarked-users.query';
+import { verifyEmail } from './verify-email.query';
 
 // mutations
 import { logIn } from './log-in.mutation';
 import { logOut } from './log-out.mutation';
 import { signUp } from './sign-up.mutation';
 import { deleteOrganization } from './delete-organization.mutation';
+
 // auth endpoints
 export const authApi = endpoints({
     getLoggedInUser,
     getBookmarkedUsers,
+    verifyEmail,
 
     // mutations
     signUp,
