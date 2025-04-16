534fdddfed4ca4a53a878e26325373cf597539f8
hernan <hernan.fg96x@gmail.com>
2025-04-16 20:44:27 +0200

    log-in route remove unused loader for bookmarked users

--- praxis-notes-app/routes/_public/auth/log-in.tsx
+++ praxis-notes-app/routes/_public/auth/log-in.tsx
@@ -2,10 +2,6 @@ import { z } from 'zod';
 
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
-import { toast } from 'sonner';
-
-import { apiClientUtils } from '@api/providers/web';
-
 import { LogInView } from '@shared/views/log-in.view';
 
 // ----------------------------------------------------------------------
@@ -33,26 +29,6 @@ export const Route = createFileRoute('/_public/auth/log-in')({
 
         // redirect to return-to path
         throw redirect({ to: returnToClean });
-    },
-
-    // loader only runs if beforeLoad doesn't throw redirect
-    loader: async () => {
-        // retrive bookmarked users
-        const bookmarkedUsersResult =
-            await apiClientUtils.auth.getBookmarkedUsers.ensureData();
-
-        const { error } = bookmarkedUsersResult;
-
-        if (error) {
-            toast.error('Failed to retrieve bookmarked users');
-
-            return { bookmarkedUsers: null };
-        }
-
-        const { data: bookmarkedUsers } = bookmarkedUsersResult;
-
-        // add bookmarked users to route loader data
-        return { bookmarkedUsers };
     },
 
     component: LogInView,
