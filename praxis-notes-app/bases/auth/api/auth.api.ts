c3a9657e3dd7aa8893f8cb25238e09b9675138da
hernan <hernan.fg96x@gmail.com>
2025-04-16 20:41:11 +0200

    verify-email mutation return email and fix filename extension

--- praxis-notes-app/bases/auth/api/auth.api.ts
+++ praxis-notes-app/bases/auth/api/auth.api.ts
@@ -3,12 +3,12 @@ import { endpoints } from '../../api/providers/server';
 // queries
 import { getLoggedInUser } from './get-logged-in-user.query';
 import { getBookmarkedUsers } from './get-bookmarked-users.query';
-import { verifyEmail } from './verify-email.query';
 
 // mutations
+import { signUp } from './sign-up.mutation';
+import { verifyEmail } from './verify-email.mutation';
 import { logIn } from './log-in.mutation';
 import { logOut } from './log-out.mutation';
-import { signUp } from './sign-up.mutation';
 import { deleteOrganization } from './delete-organization.mutation';
 
 // auth endpoints
