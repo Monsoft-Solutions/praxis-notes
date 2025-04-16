4c1c734ecf512062934d66473ef43b3d61c38c71
Adriano Flechilla <adrianof.dev@gmail.com>
2025-04-16 15:54:48 -0400

    feat(location): add location seed export and update count to zero to we don't create locations

--- praxis-notes-app/app/seed/app.seed.ts
+++ praxis-notes-app/app/seed/app.seed.ts
@@ -7,3 +7,4 @@ export * from '@src/behavior/seed';
 export * from '@src/intervention/seed';
 export * from '@src/replacement-program/seed';
 export * from '@src/client-session/seed';
+export * from '@src/location/seed';
--- praxis-notes-app/src/location/seed/location.seed.ts
+++ praxis-notes-app/src/location/seed/location.seed.ts
@@ -6,6 +6,6 @@ const _partialSchema = { locationTable };
 
 export const locationSeed: Seed<typeof _partialSchema> = () => ({
     locationTable: {
-        count: 9, // Common therapy locations: Home, School, Therapy center, Daycare, Clinic, Community center, Park, Library, Other
+        count: 0, // Common therapy locations: Home, School, Therapy center, Daycare, Clinic, Community center, Park, Library, Other
     },
 });
