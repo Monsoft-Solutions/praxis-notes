0ada7fcacc25f2c00dba165cdd8fb56b59d5c2a8
hernan <hernan.fg96x@gmail.com>
2025-04-18 15:38:11 +0200

    abc-selector component render single/multiple selector based on prop `multiple`

--- praxis-notes-app/src/client-session/components/abc-selector.component.tsx
+++ praxis-notes-app/src/client-session/components/abc-selector.component.tsx
@@ -8,7 +8,10 @@ import {
     SelectContent,
 } from '@shared/ui/select.ui';
 
+import MultipleSelector from '@shared/ui/select-multiple';
+
 type AbcSelectorProps = {
+    placeholder?: string;
     items: {
         id: string;
         name: string;
@@ -25,22 +28,37 @@ type AbcSelectorProps = {
       }
 );
 
-export function AbcSelector({ items, onSelect, multiple }: AbcSelectorProps) {
-    return (
-        <Select
-            onValueChange={
-                multiple
-                    ? (id) => {
-                          onSelect([id]);
-                      }
-                    : (id) => {
-                          onSelect(id);
-                      }
+export function AbcSelector({
+    placeholder,
+    items,
+    onSelect,
+    multiple,
+}: AbcSelectorProps) {
+    return multiple ? (
+        <MultipleSelector
+            defaultOptions={items.map((item) => ({
+                value: item.id,
+                label: item.name,
+            }))}
+            placeholder={placeholder}
+            emptyIndicator={
+                <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                    no results found.
+                </p>
             }
+            onChange={(items) => {
+                onSelect(items.map(({ value }) => value));
+            }}
+        />
+    ) : (
+        <Select
+            onValueChange={(value) => {
+                onSelect(value);
+            }}
         >
             <FormControl>
                 <SelectTrigger>
-                    <SelectValue placeholder="Select activity/antecedent" />
+                    <SelectValue placeholder={placeholder} />
                 </SelectTrigger>
             </FormControl>
 
