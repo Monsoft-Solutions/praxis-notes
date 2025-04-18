88a03f946d056ef1da246b41bc4694e90eeb9f90
hernan <hernan.fg96x@gmail.com>
2025-04-18 19:50:31 +0200

    abc-selector allow on-the-fly new item creation

--- praxis-notes-app/src/client-session/components/abc-selector.component.tsx
+++ praxis-notes-app/src/client-session/components/abc-selector.component.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { cn } from '@css/utils';
+
+import { toast } from 'sonner';
 
 import { FormControl } from '@shared/ui/form.ui';
 
 import MultipleSelector from '@shared/ui/select-multiple';
 
-import { Check } from 'lucide-react';
+import { Check, PlusCircle } from 'lucide-react';
 
 import { Button } from '@ui/button.ui';
 
@@ -23,6 +25,8 @@ import { Popover, PopoverContent, PopoverTrigger } from '@ui/popover.ui';
 
 import { Label } from '@shared/ui/label.ui';
 
+import { ConfirmationDialog } from '@shared/ui/confirmation-dialog.ui';
+
 type AbcSelectorProps = {
     placeholder?: string;
     items: {
@@ -30,6 +34,7 @@ type AbcSelectorProps = {
         name: string;
         isCustom: boolean;
     }[];
+    create: (args: { name: string }) => Promise<{ id: string } | null>;
 } & (
     | {
           multiple: true;
@@ -46,100 +51,184 @@ export function AbcSelector({
     items,
     onSelect,
     multiple,
+    create,
 }: AbcSelectorProps) {
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState<string | null>(null);
-
-    const groupBy = 'type';
-
     const options = items.map((item) => ({
         value: item.id,
         label: item.name,
         type: item.isCustom ? 'custom' : 'global',
     }));
 
-    if (multiple) {
-        return (
-            <MultipleSelector
-                defaultOptions={options}
-                groupBy={groupBy}
-                placeholder={placeholder}
-                emptyIndicator={
-                    <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
-                        no results found.
-                    </p>
-                }
-                onChange={(items) => {
-                    onSelect(items.map(({ value }) => value));
-                }}
-            />
-        );
-    }
+    const [open, setOpen] = useState(false);
+    const [createDialogOpen, setCreateDialogOpen] = useState(false);
 
-    const groupOption: Record<string, typeof options> = {};
-    options.forEach((option) => {
-        const key = option[groupBy] || '';
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        if (!groupOption[key]) {
-            groupOption[key] = [];
+    const [selectedId, setSelectedId] = useState<string | null>(null);
+    const [selectedIds, setSelectedIds] = useState<string[]>([]);
+
+    const selectedOptions = useMemo(() => {
+        return options.filter((option) => selectedIds.includes(option.value));
+    }, [options, selectedIds]);
+
+    const [search, setSearch] = useState('');
+
+    const groupBy = 'type';
+
+    const handleCreate = async ({ name }: { name: string }) => {
+        const createdItem = await create({ name });
+
+        if (!createdItem) {
+            toast.error('Failed to create custom option');
+            return;
         }
-        groupOption[key].push(option);
-    });
+
+        const { id } = createdItem;
+
+        if (multiple) {
+            const newIds = [...selectedIds, id];
+            setSelectedIds(newIds);
+            onSelect(newIds);
+        } else {
+            setSelectedId(id);
+            onSelect(id);
+        }
+
+        setCreateDialogOpen(false);
+        toast.success(`Created custom '${search}'`);
+    };
 
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-                <FormControl className="w-full">
-                    <Button
-                        variant="outline"
-                        role="combobox"
-                        className={cn(
-                            'justify-between',
-                            !value && 'text-muted-foreground',
-                        )}
-                    >
-                        {value
-                            ? items.find((item) => item.id === value)?.name
-                            : placeholder}
-                    </Button>
-                </FormControl>
-            </PopoverTrigger>
+        <>
+            {multiple ? (
+                <MultipleSelector
+                    defaultOptions={options}
+                    groupBy={groupBy}
+                    placeholder={placeholder}
+                    value={selectedOptions}
+                    creatable
+                    emptyIndicator={
+                        <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                            no results found.
+                        </p>
+                    }
+                    onChange={(selectedOptions) => {
+                        const newSelectedIds = selectedOptions.map(
+                            ({ value }) => value,
+                        );
 
-            <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
-                <Command>
-                    <CommandInput placeholder="search..." className="h-9" />
-                    <CommandList>
-                        <CommandEmpty>No framework found.</CommandEmpty>
-                        {Object.keys(groupOption).map((key) => (
-                            <CommandGroup key={key}>
-                                <Label className="ml-2">{key}</Label>
+                        const lastValue = newSelectedIds.at(-1);
 
-                                {groupOption[key].map((option) => (
+                        if (
+                            lastValue === undefined ||
+                            options.some(({ value }) => value === lastValue)
+                        ) {
+                            setSelectedIds(newSelectedIds);
+                            onSelect(newSelectedIds);
+                        } else {
+                            setSearch(lastValue);
+                            setCreateDialogOpen(true);
+                        }
+                    }}
+                />
+            ) : (
+                <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                        <FormControl className="w-full">
+                            <Button
+                                variant="outline"
+                                role="combobox"
+                                className={cn(
+                                    'justify-between',
+                                    !selectedId && 'text-muted-foreground',
+                                )}
+                            >
+                                {selectedId
+                                    ? items.find(
+                                          (item) => item.id === selectedId,
+                                      )?.name
+                                    : placeholder}
+                            </Button>
+                        </FormControl>
+                    </PopoverTrigger>
+
+                    <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
+                        <Command>
+                            <CommandInput
+                                placeholder="search..."
+                                className="h-9"
+                                value={search}
+                                onValueChange={setSearch}
+                            />
+                            <CommandList>
+                                <CommandEmpty>No framework found.</CommandEmpty>
+                                {Object.entries(
+                                    options.reduce<
+                                        Record<string, typeof options>
+                                    >((acc, option) => {
+                                        const key = option[groupBy] || '';
+                                        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+                                        if (!acc[key]) acc[key] = [];
+                                        acc[key].push(option);
+                                        return acc;
+                                    }, {}),
+                                ).map(([key, options]) => (
+                                    <CommandGroup key={key}>
+                                        <Label className="ml-2">{key}</Label>
+
+                                        {options.map((option) => (
+                                            <CommandItem
+                                                value={option.label}
+                                                key={option.value}
+                                                onSelect={() => {
+                                                    setSelectedId(option.value);
+                                                    onSelect(option.value);
+                                                    setOpen(false);
+                                                }}
+                                            >
+                                                {option.label}
+                                                <Check
+                                                    className={cn(
+                                                        'ml-auto',
+                                                        option.value ===
+                                                            selectedId
+                                                            ? 'opacity-100'
+                                                            : 'opacity-0',
+                                                    )}
+                                                />
+                                            </CommandItem>
+                                        ))}
+                                    </CommandGroup>
+                                ))}
+
+                                {search && (
                                     <CommandItem
-                                        value={option.label}
-                                        key={option.value}
                                         onSelect={() => {
-                                            setValue(option.value);
-                                            onSelect(option.value);
-                                            setOpen(false);
+                                            setCreateDialogOpen(true);
                                         }}
                                     >
-                                        {option.label}
-                                        <Check
-                                            className={cn(
-                                                'ml-auto',
-                                                option.value === value
-                                                    ? 'opacity-100'
-                                                    : 'opacity-0',
-                                            )}
-                                        />
+                                        <PlusCircle className="h-4 w-4" />
+                                        Create &quot;{search}&quot;
                                     </CommandItem>
-                                ))}
-                            </CommandGroup>
-                        ))}
-                    </CommandList>
-                </Command>
-            </PopoverContent>
-        </Popover>
+                                )}
+                            </CommandList>
+                        </Command>
+                    </PopoverContent>
+                </Popover>
+            )}
+            {/* confirmation dialog for creating custom options */}
+            <ConfirmationDialog
+                open={createDialogOpen}
+                onOpenChange={setCreateDialogOpen}
+                title="Create custom option ?"
+                description={`Do you want to create '${search}' ?`}
+                confirmLabel="Create"
+                destructive
+                onConfirm={() => {
+                    void handleCreate({ name: search });
+                }}
+                onCancel={() => {
+                    setCreateDialogOpen(false);
+                }}
+            />
+        </>
     );
 }
