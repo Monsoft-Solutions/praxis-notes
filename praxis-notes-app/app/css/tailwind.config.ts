baa8eb19eead0978dc6ddfebb9d602d6c832e244
Adriano Flechilla <adrianof.dev@gmail.com>
2025-04-22 21:17:05 -0400

    refactor(sign-up): enhance sign-up form layout and styling; update primary color variables

--- praxis-notes-app/app/css/tailwind.config.ts
+++ praxis-notes-app/app/css/tailwind.config.ts
@@ -33,7 +33,7 @@ const config: Config = {
                 background: 'hsl(var(--background))',
                 foreground: 'hsl(var(--foreground))',
                 primary: {
-                    DEFAULT: 'hsl(var(--primary))',
+                    DEFAULT: 'var(--primary)',
                     foreground: 'hsl(var(--primary-foreground))',
                 },
                 secondary: {
