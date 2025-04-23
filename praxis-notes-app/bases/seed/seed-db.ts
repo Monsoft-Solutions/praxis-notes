6240eed70501bbf04e5d31250a4749a591f8258e
hernan <hernan.fg96x@gmail.com>
2025-04-23 15:47:49 +0200

    Remove DB reset step while seeding db

--- praxis-notes-app/bases/seed/seed-db.ts
+++ praxis-notes-app/bases/seed/seed-db.ts
@@ -1,18 +1,8 @@
-import { seeder } from './seeder';
-
-import * as schema from '@db/db.tables';
-
-import { reset } from 'drizzle-seed';
-
 import * as seeds from './seeds';
 
 // function used to seed the db
 export async function seedDb() {
     console.log('seeding DB...');
-
-    console.log('resetting DB...');
-    await reset(seeder, schema);
-    console.log('DB reset');
 
     for (const seed of Object.values(seeds)) {
         await seed();
