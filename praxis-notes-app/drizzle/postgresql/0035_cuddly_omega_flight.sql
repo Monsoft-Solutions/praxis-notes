5ccf85aaca2d6aba32c74313246d510d8c6745ea
Adriano Flechilla <adrianof.dev@gmail.com>
2025-04-24 18:21:07 -0400

    Remove migration to avoid conflicts with main

--- praxis-notes-app/drizzle/postgresql/0035_cuddly_omega_flight.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "core_conf" ADD COLUMN "slack_webhook_url_error" varchar(255) NOT NULL DEFAULT '';
-ALTER TABLE "core_conf" ADD COLUMN "slack_webhook_url_info" varchar(255) NOT NULL DEFAULT '';
