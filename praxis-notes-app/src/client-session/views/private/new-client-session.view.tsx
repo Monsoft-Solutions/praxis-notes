5cf5b94453fd4a4fbce2c1c91274742db7b4e755
hernan <hernan.fg96x@gmail.com>
2025-04-30 14:04:50 +0200

    new-client-session view get initialize session-form with placeholderSessionData

--- praxis-notes-app/src/client-session/views/private/new-client-session.view.tsx
+++ praxis-notes-app/src/client-session/views/private/new-client-session.view.tsx
@@ -11,15 +11,31 @@ export function NewClientSessionView() {
         clientId,
     });
 
+    const { data: placeholderSessionDataQuery } =
+        api.clientSession.getPlaceholderSessionData.useQuery({
+            clientId,
+        });
+
     if (!clientQuery) return;
     if (clientQuery.error) return;
     const { data: client } = clientQuery;
+
+    if (!placeholderSessionDataQuery) return;
+    if (placeholderSessionDataQuery.error) return;
+    const { data: placeholderSessionData } = placeholderSessionDataQuery;
 
     const clientName = client.firstName;
 
     return (
         <div className="container mx-auto px-0 py-6">
-            <SessionForm clientId={clientId} clientName={clientName} />
+            <SessionForm
+                clientId={clientId}
+                clientName={clientName}
+                placeholderSessionData={{
+                    ...placeholderSessionData,
+                    sessionDate: new Date(placeholderSessionData.sessionDate),
+                }}
+            />
         </div>
     );
 }
