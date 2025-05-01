51c71c90b460ef9d60207d0137978c76d5b7e588
Adriano Flechilla <adrianof.dev@gmail.com>
2025-05-01 11:31:07 -0400

    Remove migration to avoid conflicts with main

--- praxis-notes-app/drizzle/postgresql/0039_bent_moondragon.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "core_conf" ADD COLUMN IF NOT EXISTS "resend_audience_id" varchar(255) NOT NULL DEFAULT '';--> statement-breakpoint
-ALTER TABLE "core_conf" ADD COLUMN IF NOT EXISTS "mailer_lite_api_key" varchar(1000) NOT NULL DEFAULT '';--> statement-breakpoint
-ALTER TABLE "core_conf" ADD COLUMN IF NOT EXISTS "mailer_lite_welcome_group_id" varchar(1000) NOT NULL DEFAULT '';
