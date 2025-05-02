de63ff07659ca3091c87aef123a5ceb7a101be2e
hernan <hernan.fg96x@gmail.com>
2025-05-02 12:46:02 +0200

    refactor generate-text provider using stream-text provider

--- praxis-notes-app/src/ai/providers/generate-text.provider.ts
+++ praxis-notes-app/src/ai/providers/generate-text.provider.ts
@@ -1,15 +1,10 @@
 import { Function } from '@errors/types';
 
-import { catchError } from '@errors/utils/catch-error.util';
 import { Error, Success } from '@errors/utils';
 
-import { getCoreConf } from '@conf/providers/server';
+import { Message } from 'ai';
 
-import { generateText as aiSdkGenerateText, Message } from 'ai';
-import { createAnthropic } from '@ai-sdk/anthropic';
-
-import { deploymentEnv } from '@env/constants/deployment-env.constant';
-import { thinkTool } from '../tools/think.tool';
+import { streamText } from './stream-text.provider';
 
 export const generateText = (async ({
     prompt,
@@ -23,45 +18,26 @@ export const generateText = (async ({
           prompt?: undefined;
           messages: Message[];
       }) => {
-    // get the core configuration
-    const coreConfWithError = await getCoreConf();
+    const { data: textStream, error: textGenerationError } = await streamText(
+        messages
+            ? {
+                  messages,
+              }
+            : { prompt },
+    );
 
-    const { error: coreConfError } = coreConfWithError;
+    if (textGenerationError) return Error();
 
-    if (coreConfError !== null) return Error('MISSING_CORE_CONF');
+    let text = '';
 
-    const { data: coreConf } = coreConfWithError;
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    while (true) {
+        const { done, value: textDelta } = await textStream.read();
 
-    const { anthropicApiKey } = coreConf;
+        if (done) break;
 
-    const anthropic = createAnthropic({
-        apiKey: anthropicApiKey,
-    });
-
-    type AnthropicModel = Parameters<typeof anthropic>[0];
-
-    const model: AnthropicModel =
-        deploymentEnv.MSS_DEPLOYMENT_TYPE === 'production'
-            ? 'claude-3-7-sonnet-latest'
-            : 'claude-3-7-sonnet-latest';
-
-    const { data: textGenerationData, error: textGenerationError } =
-        await catchError(
-            aiSdkGenerateText({
-                model: anthropic(model),
-                prompt,
-                messages,
-                tools: {
-                    think: thinkTool,
-                },
-                maxSteps: 5,
-                maxRetries: 3,
-            }),
-        );
-
-    if (textGenerationError) return Error('TEXT_GENERATION_ERROR');
-
-    const { text } = textGenerationData;
+        text += textDelta;
+    }
 
     return Success(text);
 }) satisfies Function<{ prompt: string }, string>;
