00923a52b08c4c1249c6a11c25d4fc4efc533297
hernan <hernan.fg96x@gmail.com>
2025-05-05 19:15:04 +0200

    session-details route add optional search param isGenerating, to indicate that notes for this session are being generated

--- praxis-notes-app/routes/_private/_app/clients/$clientId/sessions/$sessionId/index.tsx
+++ praxis-notes-app/routes/_private/_app/clients/$clientId/sessions/$sessionId/index.tsx
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 import { createFileRoute } from '@tanstack/react-router';
 
 import { ClientSessionDetailsView } from '@src/client-session/views/private';
@@ -5,5 +7,9 @@ import { ClientSessionDetailsView } from '@src/client-session/views/private';
 export const Route = createFileRoute(
     '/_private/_app/clients/$clientId/sessions/$sessionId/',
 )({
+    validateSearch: z.object({
+        isGenerating: z.boolean().optional(),
+    }),
+
     component: ClientSessionDetailsView,
 });
