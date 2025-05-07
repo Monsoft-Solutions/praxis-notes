0dbe4b2a13935774b3dc6d2c707d96b282fe82d0
Adriano Flechilla <adrianof.dev@gmail.com>
2025-05-07 17:31:03 -0400

    feat(ai): Introduce generateObject provider for AI object generation
    - Added a new generateObject provider to facilitate AI object generation using the Anthropic model.
    - Integrated Langfuse for tracing and monitoring AI requests.
    - Updated the index file to export the new provider.
    - Modified the generateSuggestedQuestions utility to utilize the new generateObject provider for generating question suggestions.

---
+++ praxis-notes-app/src/ai/providers/generate-object.provider.ts
@@ -0,0 +1,107 @@
+import { Function } from '@errors/types';
+
+import { Error, Success } from '@errors/utils';
+
+import { getCoreConf } from '@conf/providers/server';
+
+import {
+    generateObject as aiSdkGenerateObject,
+    GenerateObjectResult,
+} from 'ai';
+import { createAnthropic } from '@ai-sdk/anthropic';
+
+import { AiRequest } from '../schemas/ai-request.schema';
+
+import { v4 as uuidv4 } from 'uuid';
+
+import { Langfuse } from 'langfuse';
+import { ZodSchema } from 'zod';
+import { catchError } from '@errors/utils/catch-error.util';
+
+export const generateObject = (async ({
+    prompt,
+    messages,
+    modelParams,
+    outputSchema,
+}: {
+    prompt: string;
+    messages?: undefined;
+    modelParams: AiRequest;
+    outputSchema: ZodSchema;
+}) => {
+    // get the core configuration
+    const coreConfWithError = await getCoreConf();
+
+    const { error: coreConfError } = coreConfWithError;
+
+    if (coreConfError !== null) return Error('MISSING_CORE_CONF');
+
+    const { data: coreConf } = coreConfWithError;
+
+    const {
+        anthropicApiKey,
+        langfuseSecretKey,
+        langfusePublicKey,
+        langfuseBaseUrl,
+    } = coreConf;
+
+    const langfuse = new Langfuse({
+        secretKey: langfuseSecretKey,
+        publicKey: langfusePublicKey,
+        baseUrl: langfuseBaseUrl,
+    });
+
+    const traceId = uuidv4();
+
+    const trace = langfuse.trace({
+        name: 'generate_object',
+        id: traceId,
+        metadata: {
+            prompt,
+            messages,
+            modelParams,
+        },
+        sessionId: traceId,
+    });
+
+    const anthropic = createAnthropic({
+        apiKey: anthropicApiKey,
+    });
+
+    const model = modelParams.model;
+
+    const generation = trace.generation({
+        model,
+        input: prompt,
+    });
+
+    const { data: result, error } = await catchError(
+        aiSdkGenerateObject({
+            model: anthropic(model),
+            prompt,
+            schema: outputSchema,
+        }),
+    );
+
+    if (error) return Error('AI_GENERATE_OBJECT_FAILED');
+
+    generation.end({
+        input: prompt,
+        output: result.object,
+        usage: {
+            input: result.usage.promptTokens,
+            output: result.usage.completionTokens,
+            total: result.usage.totalTokens,
+        },
+    });
+
+    return Success(result.object);
+}) satisfies Function<
+    {
+        prompt: string;
+        messages?: undefined;
+        modelParams: AiRequest;
+        outputSchema: ZodSchema;
+    },
+    GenerateObjectResult<unknown>['object']
+>;
--- praxis-notes-app/src/chat/utils/generate-suggested-questions.util.ts
+++ praxis-notes-app/src/chat/utils/generate-suggested-questions.util.ts
@@ -3,10 +3,13 @@ import { z } from 'zod';
 import { Function } from '@errors/types';
 import { Error, Success } from '@errors/utils';
 
-import { generateText } from '@src/ai/providers';
-import { jsonParse } from '@shared/utils/json-parse.util';
+import { generateObject } from '@src/ai/providers';
 
-const questionSuggestionSchema = z.array(z.string());
+const questionSuggestionSchema = z.object({
+    questions: z.array(z.string()),
+});
+
+type QuestionSuggestionSchema = z.infer<typeof questionSuggestionSchema>;
 
 /**
  * Generates four suggested questions/instructions related to ABA (Applied Behavior Analysis)
@@ -27,8 +30,6 @@ export const generateSuggestedQuestions = (async ({
     The questions/instructions should be clear, specific, and invite detailed responses. They should be appropriate for 
     someone interested in learning about or implementing ABA therapy.
 
-    Format the response as a simple JSON array with exactly 4 strings, nothing else.
-
     The questions/instructions should be in the user's preferred language.
 
     The questions/instructions should have a maximum of 20 words each.
@@ -36,31 +37,23 @@ export const generateSuggestedQuestions = (async ({
     User's preferred language: ${userLanguage}
     User's name: ${userName}`;
 
-    const { data: generatedText, error: generateTextError } =
-        await generateText({
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { data: generatedObject, error: generateObjectError } =
+        await generateObject({
             prompt,
             modelParams: {
                 provider: 'anthropic',
                 model: 'claude-3-haiku-20240307',
                 activeTools: [],
             },
+            outputSchema: questionSuggestionSchema,
         });
 
-    if (generateTextError) return Error('AI_COMPLETION_FAILED');
+    if (generateObjectError) return Error('AI_COMPLETION_FAILED');
 
-    const { data: json, error: jsonError } = jsonParse(generatedText);
+    const array = (generatedObject as QuestionSuggestionSchema).questions;
 
-    if (jsonError) return Error('INVALID_JSON_RESPONSE');
-
-    const parsedResponse = questionSuggestionSchema.safeParse(json);
-
-    if (!parsedResponse.success) {
-        return Error('INVALID_RESPONSE_FORMAT');
-    }
-
-    const { data: questions } = parsedResponse;
-
-    return Success(questions);
+    return Success(array);
 }) satisfies Function<
     {
         userName: string;
