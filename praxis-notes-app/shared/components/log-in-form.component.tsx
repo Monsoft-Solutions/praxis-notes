4b6f7708654766063f57575834e9d6d617a2193d
hernan <hernan.fg96x@gmail.com>
2025-05-12 20:17:42 +0200

    refactor log-in-form as shared component

--- praxis-notes-app/shared/components/log-in-form.component.tsx
+++ praxis-notes-app/shared/components/log-in-form.component.tsx
@@ -2,8 +2,6 @@ import { ReactElement } from 'react';
 
 import { Link } from '@tanstack/react-router';
 
-import { authClient } from '@auth/providers/web/auth-client.provider';
-
 import type { UseFormReturn } from 'react-hook-form';
 
 import type { LogInCredentials } from '@auth/schemas';
@@ -29,7 +27,7 @@ import {
     CardTitle,
 } from '@ui/card.ui';
 
-import { FaGoogle } from 'react-icons/fa';
+import { SocialLogIn } from './social-log-in.component';
 
 // ----------------------------------------------------------------------
 
@@ -114,31 +112,7 @@ export function LogInForm({
                     </form>
                 </Form>
 
-                <div className="relative mt-6">
-                    <div className="absolute inset-0 flex items-center">
-                        <span className="w-full border-t" />
-                    </div>
-
-                    <div className="relative flex justify-center text-xs uppercase">
-                        <span className="bg-background text-muted-foreground px-2">
-                            Or continue with
-                        </span>
-                    </div>
-                </div>
-
-                <Button
-                    variant="outline"
-                    type="button"
-                    className="mt-6 h-11 w-full font-medium"
-                    onClick={() => {
-                        void authClient.signIn.social({
-                            provider: 'google',
-                        });
-                    }}
-                >
-                    <FaGoogle className="mr-2 h-4 w-4" />
-                    Google
-                </Button>
+                <SocialLogIn />
             </CardContent>
 
             <CardFooter className="flex justify-center border-t p-4">
