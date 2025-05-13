66f7f66af789e9900a0f098a2b1c915cdcc5a54f
Adriano Flechilla <adrianof.dev@gmail.com>
2025-05-13 11:04:07 -0400

    chore(database): remove DB related changes to merge with main

--- praxis-notes-app/drizzle/postgresql/0053_absurd_romulus.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "reinforcer" (
-	"id" char(36) PRIMARY KEY NOT NULL,
-	"organization_id" char(36),
-	"name" varchar(255) NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "reinforcer" ADD CONSTRAINT "reinforcer_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;
