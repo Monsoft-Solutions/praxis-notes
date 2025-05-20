62b757b903ad56a7a25fe4e2d6b65f0765e7de13
hernan <hernan.fg96x@gmail.com>
2025-05-20 18:22:17 +0200

    get-ai-sdk-model-from-name provider

---
+++ praxis-notes-app/src/ai/providers/get-ai-sdk-model-from-name.provider.ts
@@ -0,0 +1,30 @@
+import { Function } from '@errors/types';
+import { Success, Error } from '@errors/utils';
+
+import { LanguageModelV1 } from 'ai';
+
+import { ModelName } from '../enums';
+
+import { getAiProviderNameFromModelName } from './get-ai-provider-name-from-model-name.provider';
+
+import { getAiSdkProviderFromName } from './get-ai-sdk-provider-from-name.provider';
+
+export const getAiSdkModelFromName = (async ({ modelName }) => {
+    const { data: providerName, error: providerNameError } =
+        getAiProviderNameFromModelName({
+            modelName,
+        });
+
+    if (providerNameError) return Error('INVALID_MODEL_NAME');
+
+    const { data: provider, error: providerError } =
+        await getAiSdkProviderFromName({
+            providerName,
+        });
+
+    if (providerError) return Error('INVALID_MODEL_NAME');
+
+    const model = provider(modelName);
+
+    return Success(model);
+}) satisfies Function<{ modelName: ModelName }, LanguageModelV1>;
